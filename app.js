f153efa2b31293eb4f1799dc2aa37c29da6b774a
Leif Larsen <leif.larsen@novanet.no>
2020-06-08 14:01:03 +0200

    Some refactoring done

--- app.js
+++ app.js
@@ -7,59 +7,82 @@ new Vue({
         gameLog: []
     },
     methods: {
-        giveUp: function() {
-            if(confirm("Are you sure you want to give up?")) {
-                this.restartGame();
-            }
-        },
-        attack: function() {
-            this.playerAttack(1, 5);
-            this.monsterAttack(1, 5);
-            this.checkStatus();
-        },
-        specialAttack: function() {
-            this.playerAttack(5, 20);
-            this.monsterAttack(5, 20);
-            this.checkStatus();
-        },
-        heal: function() {
-            var healthGain = this.random(5, 10);
-            this.playerHealth += healthGain;
-            this.gameLog.push({"player": "Player used heal and gained " + healthGain + " HP"});
-            
-            this.monsterAttack(1, 5);
-            
-            this.checkStatus();
-        },
-        checkStatus: function() {
-            if(this.playerHealth <= 0 && this.monsterHealth > 0) {
-                if(confirm("You lost. \r\n Do you want to play another game?")) {
-                    this.restartGame();
-                }
-            } else if(this.playerHealth > 0 && this.monsterHealth <= 0) {
-                if(confirm("You won. \r\n Do you want to play another game?")) {
-                    this.restartGame();
-                }
-            }
-        },
-        random: function(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        },
-        restartGame: function() {
+        startGame: function() {
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.gameLog.splice(0, this.gameLog.length);
-            this.inGame = false;
+            this.inGame = true;
+        },
+        attack: function() {
+            this.playerAttack(1, 5);
+            this.checkWin();
+
+            this.monsterAttack(1, 5);
+            this.checkWin();
+        },
+        specialAttack: function() {
+            this.playerAttack(5, 20);
+            this.checkWin();
+
+            this.monsterAttack(5, 20);
+            this.checkWin();
+        },
+        heal: function() {
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.gameLog.unshift({ 
+                isPlayer: true,
+                text: "Player used heal"
+            });
+            
+            this.monsterAttack(1, 5);
+            
+            this.checkStatus();
+        },
+        giveUp: function() {
+            if(confirm("Are you sure you want to give up?")) {
+                this.startGame();
+            }
+        },
+
+        calculateDamage: function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        checkWin: function() {
+            if(this.playerHealth <= 0 && this.monsterHealth > 0) {
+                if(confirm("You lost. \r\nNew game?")) {
+                    this.startGame();
+                } else {
+                    this.inGame = false;
+                }
+            } else if(this.playerHealth > 0 && this.monsterHealth <= 0) {
+                if(confirm("You won. \r\nNew game?")) {
+                    this.startGame();
+                } else {
+                    this.inGame = false;
+                }
+            }
+
+            return false;
         },
         monsterAttack: function(min, max) {
-            var playerHit = this.random(1, 5);
+            var playerHit = this.calculateDamage(1, 5);
             this.playerHealth -= playerHit;
-            this.gameLog.push({"monster": "Monster used attack and took " + playerHit + " HP"});
+            this.gameLog.unshift({
+                isPlayer: false,
+                text: "Monster used attack and took " + playerHit + " HP"
+            });
         },
         playerAttack: function(min, max) {
-            var monsterHit = this.random(5, 20);
+            var monsterHit = this.calculateDamage(5, 20);
             this.monsterHealth -= monsterHit;
-            this.gameLog.push({"player": "Player used attack and took " + monsterHit + " HP"});
+            this.gameLog.unshift({ 
+                isPlayer: true,
+                text: "Player used attack and took " + monsterHit + " HP"
+            });
         }
     },
     computed: {
