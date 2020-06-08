2706b6b25742b4b3bea72df556affdf72b76bcf0
Leif Larsen <leif.larsen@novanet.no>
2020-06-08 12:03:54 +0200

    Setup main functionality

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 new Vue({
     el: "#app",
     data: {
-        hello: "Hello, World!"
+        inGame: false,
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameLog: []
+    },
+    methods: {
+        giveUp: function() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.gameLog.splice(0, this.gameLog.length);
+            this.inGame = false;
+        },
+        attack: function() {
+            this.monsterHealth -= 5;
+            this.gameLog.push("Player used attack");
+        },
+        specialAttack: function() {
+            this.monsterHealth -= 20;
+            this.gameLog.push("Player used super attack");
+        },
+        heal: function() {
+            this.gameLog.push("Player used heal");
+        }
+    },
+    computed: {
+        playerHealthBar: function() {
+            return {
+                width: this.playerHealth + '%'
+            }
+        },
+        monsterHealthBar: function() {
+            return {
+                width: this.monsterHealth + '%'
+            }
+        }
     }
 });
